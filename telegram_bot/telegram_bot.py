0ca6dea0d0b2e69605ade4ecb0b3708f28d1c854
AlessandroRedi <alessandroredi1@gmail.com>
2023-03-01 17:29:14 +0100

    Added the callback query function and the buttons

--- telegram_bot/telegram_bot.py
+++ telegram_bot/telegram_bot.py
@@ -12,7 +12,8 @@ class Telegram_Bot:
         conf = json.load(open("C:\\Users\\aledi\\Desktop\\Alessandro\\PoliTo\\Magistrale\\Programming_IOT\\Lab5\\setting.json"))
         self.tokenBot = conf["token"]
         self.bot = telepot.Bot(self.tokenBot)
-        MessageLoop(self.bot, {'chat': self.on_chat_message}).run_as_thread()
+        MessageLoop(self.bot, {'chat': self.on_chat_message,
+                               'callback_query' : self.on_callback_query}).run_as_thread()
 
     def on_chat_message(self, msg): 
         """This is actually the main of the bot"""
@@ -24,8 +25,13 @@ class Telegram_Bot:
             self.help(chat_ID)
         elif message == "/start":
             self.start(chat_ID)
-        elif message == "/status":
-            self.bot.sendMessage(chat_ID, text="C'mon do something")
+        elif message == "/add":
+            buttons = [[InlineKeyboardButton(text=f'Add User', callback_data=f'Add_User'),
+                        InlineKeyboardButton(text=f'Add Plant', callback_data=f'Add_Plant'),
+                        InlineKeyboardButton(text=f'Add GreenHouse', callback_data=f'Add_Greenhouse'),
+                        InlineKeyboardButton(text=f'Add lot', callback_data=f'Add_lot')]]
+            keyboard = InlineKeyboardMarkup(inline_keyboard=buttons)
+            self.bot.sendMessage(chat_ID, text='Add Command List', reply_markup=keyboard)
         elif message == "/selectGreenhouse":
             self.bot.sendMessage(chat_ID, text="C'mon do something")
         elif message == "/values":
@@ -36,12 +42,22 @@ class Telegram_Bot:
             self.bot.sendMessage(chat_ID, text="C'mon do something")
         elif message.startswith('/'):
             self.bot.sendMessage(chat_ID, text="Command not supported")
+            
+    def on_callback_query(self, msg):
+            query_ID, chat_ID, query_data = telepot.glance(msg, flavor='callback_query') #query_data is the callback data write in the buttons
+            action, place = query_data.split('_')
+            if action == 'Add':
+                if place == 'Plant':
+                    self.bot.sendMessage(chat_ID, text=f"Insert the name of the new plant")
+                elif place == '':
+                    pass
     
     def help(self, chat_ID):
         """Send a message when the command /help is received."""
 
-        help_message = ("You can perform the following actions:\n"
+        help_message = ("*You can perform the following actions:*\n"
                     "- /status: Get info about your greenhouses\n"
+                    "- /add: Add new component\n"
                     "- /selectGreenhouse: Get ID info about your greenhouse\n"
                     "- /values: Get environmental info for your plant\n"
                     "- /plant: Get info about your plants\n"
