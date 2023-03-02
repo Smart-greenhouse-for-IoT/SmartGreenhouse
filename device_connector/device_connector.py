1d469fd346cb28f9da39cb038692615345e384c3
AlessandroRedi <alessandroredi1@gmail.com>
2023-03-02 17:19:49 +0100

    added a function and changed mqtt initialization

--- device_connector/device_connector.py
+++ device_connector/device_connector.py
@@ -12,8 +12,13 @@ class Device_Connector(object):
     """Get data from catalog"""
 
     def __init__(self):
-        self.conf = json.load(open("setting.json"))
-        return
+        self.conf = json.loads(open("setting.json"))
+        self.devices = json.loads(open("devices.json"))
+        broker_dict = self.get_broker()
+        self.client_mqtt = MyMQTT(broker_dict["clientID"],broker_dict["IP"],broker_dict["port"],self)
+
+    def notify(self,topic,payload): 
+        """Where we receive the topic which we are subscribed"""
 
     def get_broker(self): 
         """GET all the broker information"""
@@ -21,48 +26,27 @@ class Device_Connector(object):
         b_dict = json.loads(requests.get(string))  #GET from catalog #need a .text /.body?
         return b_dict #return a json dict with BrokerIP and BrokerPort
     
-    def get_broker_topic(self): 
+    def get_broker_topic(self,id): 
         """GET all the topic needed for sub and pub"""
-        string = "http://" + self.conf["CatIP"] + ":" + self.conf["CatPort"] + "/topics" #URL for GET
-        top_dict = json.loads(requests.get(string))  #GET from catalog the topics that i have to subscribe and publish form 
+        string = f"http://" + self.conf["CatIP"] + ":" + self.conf["CatPort"] + "/topics" + "/" + {id} #URL for GET
+        top_dict = json.loads(requests.get(string))  #GET from catalog the topics that i have to subscribe and publish
         return top_dict
-
-class pub():
-    def __init__(self, clientID, broker, port, topic):
-        self.broker = broker
-        self.port = port
-        self.topic = topic
-        self.clientID = clientID
-        self.client = MyMQTT(self.clientID, self.broker, self.port,self)
     
-    def startPub(self):
-        """Starting the publisher"""
-        self.client.start()
-        time.sleep(3) #we want to be sure to do that commands in order
-        
-    def pubEvent(self):
-        """Publish event"""
-        self.client.myPublish(self.topic)
+    def post_sensor_Cat(self,id): 
+        """Post to the catalog all the sensors of this device connector"""
+        string = f"http://" + self.conf["CatIP"] + ":" + self.conf["CatPort"] + "/updateSensors" #URL for POST
+        requests.post(string, json = self.devices)
+    
+    def humiditySens(self):
 
-class sub():
-    def __init__(self, clientID, broker, port, topic):
-        self.broker = broker
-        self.port = port
-        self.topic = topic
-        self.clientID = clientID
-        self.client = MyMQTT(self.clientID, self.broker, self.port,self)
+        pass
 
-    def startSub(self):
-        """Starting the subscriber"""
-        self.client.start()
-        time.sleep(3) #we want to be sure to do that commands in order
-        self.client.mySubscribe(self.topic) #subscribing to the selected topic
+    def irrigator(self, action):
 
-    def notify(self,topic,payload):
-        """Receiving the topic"""
-        
+        pass
+
 
 if __name__=='__main__':
-
+    dc = Device_Connector()
     while True:
         time.sleep(1)
