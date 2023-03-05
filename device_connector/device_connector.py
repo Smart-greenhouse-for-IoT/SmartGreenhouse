c92989c7a0fdb3ff89bee5a9fb552c4f8cca4645
AlessandroRedi <alessandroredi1@gmail.com>
2023-03-05 11:17:52 +0100

    Added a way to add things to the catalog

---
+++ device_connector/device_connector.py
@@ -0,0 +1,60 @@
+# Device Connector : 28/02/2022
+
+import requests
+import json
+from MyMQTT import *
+import sys
+import time
+import datetime
+import paho.mqtt.client as PahoMQTT
+
+class Device_Connector(object):
+    """Device connector class:
+            - get info from the catalog"""
+
+    def __init__(self):
+        self.conf = json.load(open("device_connector\\conf.json"))
+        self.devices = json.load(open("device_connector\\devices.json"))
+        broker_dict = self.get_broker()
+        print(broker_dict)
+        self.client_mqtt = MyMQTT(broker_dict["clientID"],broker_dict["IP"],broker_dict["port"],self)
+        self.client_mqtt.start()
+        time.sleep(3)
+
+    def notify(self,topic,payload): 
+        """Where we receive the topic which we are subscribed (plant control microservices)"""
+        self.actuation = json.load(payload)
+        if self.actuation["action"] == True:
+            self.irrigator() #TODO: bisogna passargli il lotto a cui irrigare, lo prendo dal topic???
+
+    def get_broker(self): 
+        """GET all the broker information"""
+        string = "http://" + self.conf["CatIP"] + ":" + self.conf["CatPort"] + "/broker" #URL for GET
+        b_dict = requests.get(string).json()  #GET from catalog #need a .text /.body?
+        return b_dict #return a json dict with BrokerIP and BrokerPort
+    
+    def get_broker_topic(self,id): 
+        """GET all the topic needed for sub and pub"""
+        string = f"http://" + self.conf["CatIP"] + ":" + self.conf["CatPort"] + "/topics" + "/" + {id} #URL for GET
+        top_dict = requests.get(string).json()  #GET from catalog the topics that i have to subscribe and publish
+        return top_dict
+    
+    def post_sensor_Cat(self,id): 
+        """Post to the catalog all the sensors of this device connector"""
+        string = f"http://" + self.conf["CatIP"] + ":" + self.conf["CatPort"] + "/updateSensors" #URL for POST
+        requests.post(string, json = self.devices)
+    
+    def humiditySens(self):
+        #TODO: read the simulated values from a json (in senml format?)
+        pass
+
+    def irrigator(self, lot):
+        print(f"Irrigation started on lot : {lot}")
+    
+    def main():
+        pass
+
+if __name__=='__main__':
+    dc = Device_Connector()
+    while True:
+        time.sleep(30)
--- telegram_bot/telegram_bot.py
+++ telegram_bot/telegram_bot.py
@@ -9,8 +9,8 @@ class Telegram_Bot:
     """Some response of the bot"""
 
     def __init__(self):
-        conf = json.load(open("C:\\Users\\aledi\\Desktop\\Alessandro\\PoliTo\\Magistrale\\Programming_IOT\\Lab5\\setting.json"))
-        self.tokenBot = conf["token"]
+        self.conf = json.load(open("telegram_bot\\setting.json"))
+        self.tokenBot = self.conf["token"]
         self.bot = telepot.Bot(self.tokenBot)
         MessageLoop(self.bot, {'chat': self.on_chat_message,
                                'callback_query' : self.on_callback_query}).run_as_thread()
@@ -21,44 +21,72 @@ class Telegram_Bot:
         #if chat_ID not in self.chatIDs:   # this line maybe is needed for security????
         #    self.chatIDs.append(chat_ID)
         message = msg['text']
-        if message == "/help": #choosing the action to do based on the command received
-            self.help(chat_ID)
-        elif message == "/start":
-            self.start(chat_ID)
-        elif message == "/add":
-            buttons = [[InlineKeyboardButton(text=f'Add User', callback_data=f'Add_User'),
-                        InlineKeyboardButton(text=f'Add Plant', callback_data=f'Add_Plant'),
-                        InlineKeyboardButton(text=f'Add GreenHouse', callback_data=f'Add_Greenhouse'),
-                        InlineKeyboardButton(text=f'Add lot', callback_data=f'Add_lot')]]
-            keyboard = InlineKeyboardMarkup(inline_keyboard=buttons)
-            self.bot.sendMessage(chat_ID, text='Add Command List', reply_markup=keyboard)
-        elif message == "/selectGreenhouse":
-            self.bot.sendMessage(chat_ID, text="C'mon do something")
-        elif message == "/values":
-            self.bot.sendMessage(chat_ID, text="C'mon do something")
-        elif message == "/plant":
-            self.bot.sendMessage(chat_ID, text="C'mon do something")
-        elif message == "/irrigate":
-            self.bot.sendMessage(chat_ID, text="C'mon do something")
-        elif message.startswith('/'):
-            self.bot.sendMessage(chat_ID, text="Command not supported")
+
+        if (message.find('_')>0): #This mean that i have the information that the command is carrying
+            command, Name, userID = message.split('_') 
+
+            if command == "/addUser":            
+                self.newUser = {"username": Name,
+                                "userID": userID}
+                buttons = [[InlineKeyboardButton(text=f'Yes', callback_data=f'Add_User_True'), #TODO:trovare un modo per togliere la selezione grafica del pulsante su telegram(davvero brutto da vedere)
+                            InlineKeyboardButton(text=f'No', callback_data=f'Add_User_False')]]
+                keyboard = InlineKeyboardMarkup(inline_keyboard=buttons)
+                self.bot.sendMessage(chat_ID, text=f"The new user is:\n {self.newUser}") #TODO: scriverlo in un modo più carino???
+                self.bot.sendMessage(chat_ID, text='Is the data correct?', reply_markup=keyboard)
+
+            elif command == "/addPlant":
+                pass
+        else:
+            if message == "/help": #choosing the action to do based on the command received
+                self.help(chat_ID)
+
+            elif message == "/start": #welcome message
+                self.start(chat_ID)
+
+            elif message == "/addUser":
+                text_Add = ("To add a new user write in this way:\n"
+                            "/addUser_Name_ID \n")
+                self.bot.sendMessage(chat_ID, text=text_Add)
+            elif message == "/selectGreenhouse":
+                self.bot.sendMessage(chat_ID, text="C'mon do something")
+            elif message == "/values":
+                self.bot.sendMessage(chat_ID, text="C'mon do something")
+            elif message == "/plant":
+                self.bot.sendMessage(chat_ID, text="C'mon do something")
+            elif message == "/irrigate":
+                self.bot.sendMessage(chat_ID, text="C'mon do something")
+            elif message.startswith('/'):
+                self.bot.sendMessage(chat_ID, text="Command not supported")
             
     def on_callback_query(self, msg):
-            query_ID, chat_ID, query_data = telepot.glance(msg, flavor='callback_query') #query_data is the callback data write in the buttons
-            action, place = query_data.split('_')
-            if action == 'Add':
-                if place == 'Plant':
-                    self.bot.sendMessage(chat_ID, text=f"Insert the name of the new plant")
-                    self.plantname = True
-                elif place == '':
-                    pass
-    
+        """callback query function:
+                - it will elabrate the information when a button is pressed"""
+        
+        query_ID, chat_ID, query_data = telepot.glance(msg, flavor='callback_query') #query_data is the callback data write in the buttons
+        action, place, doing = query_data.split('_')
+
+        if action == 'Add': #Action of adding something to the catalog
+            if place == 'Plant':
+                pass
+
+            elif place == 'User': #Adding a new user to the catalog
+                if doing == "True":
+                    string = f"http://" + self.conf["CatIP"] + ":" + self.conf["CatPort"] + "/addUser" #URL for POST #TODO: we need a post or a put???
+                    requests.post(string, json = self.newUser)
+                    self.bot.sendMessage(chat_ID, text=f"User added correctly")
+                elif doing == "False":
+                    self.bot.sendMessage(chat_ID, text=f"User not added, please repeat the command")
+            
+            else:
+                pass
+
     def help(self, chat_ID):
         """Send a message when the command /help is received."""
 
-        help_message = ("*You can perform the following actions:*\n"
+        help_message = ("*You can perform the following actions:*\n" #TODO: comandi help fatti un po a caso, sono tutti da decidere
                     "- /status: Get info about your greenhouses\n"
-                    "- /add: Add new component\n"
+                    "- /addUser: Add new user\n"
+                    "- /addPlant: Add new plant\n" #TODO: decidere se si possono aggiungere piante nuove o se cerchiamo un database già fatto
                     "- /selectGreenhouse: Get ID info about your greenhouse\n"
                     "- /values: Get environmental info for your plant\n"
                     "- /plant: Get info about your plants\n"
