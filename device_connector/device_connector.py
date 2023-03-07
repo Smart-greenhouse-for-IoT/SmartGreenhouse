766793ed9d6eaa3d695b1e94e8549107ff6ee895
AlessandroRedi <alessandroredi1@gmail.com>
2023-03-07 10:18:34 +0100

    Added the time module to do the function in a continue way
    Added also the some mqtt methods and correct the json file devices.json

--- device_connector/device_connector.py
+++ device_connector/device_connector.py
@@ -10,48 +10,77 @@ import paho.mqtt.client as PahoMQTT
 
 class Device_Connector(object):
     """Device connector class:
-            - get info from the catalog"""
+            - get info from the catalog
+            - """
 
     def __init__(self):
-        self.conf = json.load(open("device_connector/conf.json"))
-        self.devices = json.load(open("device_connector/devices.json"))
+
+        self.conf = json.load(open("device_connector\\conf.json"))
+        self.devices = json.load(open("device_connector\\devices.json"))
         broker_dict = self.get_broker()
-        self.client_mqtt = MyMQTT(broker_dict["clientID"],broker_dict["IP"],broker_dict["port"], self)
-        self.client_mqtt.start()
+        print(broker_dict)
+        self.client_mqtt = MyMQTT(broker_dict["clientID"],broker_dict["IP"],broker_dict["port"],self)
 
     def notify(self,topic,payload): 
         """Where we receive the topic which we are subscribed (plant control microservices)"""
+
         self.actuation = json.load(payload)
         if self.actuation["action"] == True:
             self.irrigator() #TODO: bisogna passargli il lotto a cui irrigare, lo prendo dal topic???
 
+    def subscribe(self):
+        """subscriber to all the irrigator topics of this device connector"""
+
+        self.client_mqtt.start()
+        time.sleep(3) #we want to be sure to do that commands in order
+        self.client_mqtt.mySubscribe()
+
     def get_broker(self): 
         """GET all the broker information"""
+
         string = "http://" + self.conf["CatIP"] + ":" + self.conf["CatPort"] + "/broker" #URL for GET
         b_dict = requests.get(string).json()  #GET from catalog #need a .text /.body?
         return b_dict #return a json dict with BrokerIP and BrokerPort
     
-    def get_broker_topic(self,id): 
-        """GET all the topic needed for sub and pub"""
-        string = f"http://" + self.conf["CatIP"] + ":" + self.conf["CatPort"] + "/topics" + "/" + {id} #URL for GET
-        top_dict = requests.get(string).json()  #GET from catalog the topics that i have to subscribe and publish
-        return top_dict
+    #def get_broker_topic(self,id): 
+    #    """GET all the topic needed for sub and pub"""
+    #    string = f"http://" + self.conf["CatIP"] + ":" + self.conf["CatPort"] + "/topics" + "/" + {id} #URL for GET
+    #    top_dict = requests.get(string).json()  #GET from catalog the topics that i have to subscribe and publish
+    #    return top_dict
     
     def post_sensor_Cat(self): 
         """Post to the catalog all the sensors of this device connector"""
-        string = f"http://" + self.conf["CatIP"] + ":" + self.conf["CatPort"] + "/updateDevices" #URL for POST
-        requests.post(string, json = self.devices)
+
+        self.localtime = datetime.datetime.now() # get struct_time
+        if (self.localtime-self.post_senstor_cat_Time) > datetime.timedelta(seconds=30):
+            string = f"http://" + self.conf["CatIP"] + ":" + self.conf["CatPort"] + "/updateSensors" #URL for POST
+            requests.post(string, json = self.devices)
+        self.post_senstor_cat_Time = self.localtime
+        
     
     def humiditySens(self):
-        
-        pass
+        """Get the simulated value of the humidity sensor and publish it to the correspondent topic"""
+
+        self.localtime = datetime.datetime.now() # get struct_time
+        if (self.localtime-self.humidity_Time) > datetime.timedelta(minutes=1):
+            for dv in self.devices["devicesList"]:
+                if dv["type"] == "sensor":
+                    #TODO: get the humidity level
+                    #humidity = leggo da un file json?
+                    #self.client_mqtt.myPublish(dv["topic"], humidity) #TODO: quante cose devo mandare? basta topic e umidità?
+                    pass
+        self.humidity_Time = self.localtime
 
     def irrigator(self, lot):
-        print(f"Irrigation started on lot : {lot}")
+        #TODO: pubblicare al topic dell'attuatore dell'irrigazione?
+        pass
 
 
 if __name__=='__main__':
+
     dc = Device_Connector()
-    dc.post_sensor_Cat()
+
     while True:
-        time.sleep(1)
+        #dc.humiditySens()
+        dc.post_sensor_Cat()
+        time.sleep(5)
